6a0c22b36fb677155e956a02eee9ceaede0bd87b
Ayko Sousa do Nascimento <ayko.souza74@gmail.com>
2024-05-30 10:55:34 -0300

    Feat: Add custom logger

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"github.com/AykoSousa/go-opportunities/config"
 	"github.com/AykoSousa/go-opportunities/router"
 )
 
+var (
+	logger config.Logger
+)
+
 func main() {
+
+	logger = *config.GetLogger("main")
+
+	// Initialize configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config initialiation error: %v", err)
+		return
+	}
+	// Initialize routes
 	router.Initialize()
 }
