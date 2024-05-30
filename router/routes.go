bd372894763d1e2038e1a17a4ebad50f1b727a93
Ayko Sousa do Nascimento <ayko.souza74@gmail.com>
2024-05-30 09:17:58 -0300

    Feat: Add methods DELETE, POST, PUT in /opening routes

--- router/routes.go
+++ router/routes.go
@@ -15,6 +15,28 @@ func initializeRoutes(router *gin.Engine) {
 			})
 		})
 
-		
+		v1.POST("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "POST opening",
+			})
+		})
+
+		v1.DELETE("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "DELETE opening",
+			})
+		})
+
+		v1.PUT("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "PUT opening",
+			})
+		})
+
+		v1.GET("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "GET openings",
+			})
+		})
 	}
 }
