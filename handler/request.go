cc3361ac57e8a3dd5394b876997e683bbace23d2
Ayko Sousa do Nascimento <ayko.souza74@gmail.com>
2024-05-30 13:44:45 -0300

    Feat: Add request and response helpers

---
+++ handler/request.go
@@ -0,0 +1,44 @@
+package handler
+
+import "fmt"
+
+func errParamIsRequired(name, typ string) error {
+	return fmt.Errorf("Param: %s (type: %s) is required", name, typ)
+}
+
+// CreateOpening
+
+type CreateOpeningRequest struct {
+	Role     string  `json: "role"`
+	Company  string  `json: "company"`
+	Location string  `json: "location"`
+	Remote   *bool   `json: "remote"`
+	Link     string  `json: "link"`
+	Salary   float64 `json: "salary"`
+}
+
+func (request *CreateOpeningRequest) Validate() error {
+	if request.Role == "" && request.Company == "" && request.Location == "" && request.Link == "" && request.Remote == nil && request.Salary <= 0 {
+		return fmt.Errorf("Params body is empty or malformed")
+	}
+	if request.Role == "" {
+		return errParamIsRequired("role", "string")
+	}
+	if request.Company == "" {
+		return errParamIsRequired("company", "string")
+	}
+	if request.Location == "" {
+		return errParamIsRequired("location", "string")
+	}
+	if request.Link == "" {
+		return errParamIsRequired("link", "string")
+	}
+	if request.Remote == nil {
+		return errParamIsRequired("remote", "bool")
+	}
+	if request.Salary <= 0 {
+		return errParamIsRequired("salary", "float64")
+	}
+	
+	return nil
+}
